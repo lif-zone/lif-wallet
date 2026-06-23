eefa92cb753f4eefecee29189a57b6e4e2de57e3
Bright Life <bright@bright.center>
2026-06-23 17:22:24 +0300

    cosmetic

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 import server from 'lif-kernel/server_lib.js';
 let root = import.meta.dirname;
 let map = {};
-map['/lif-kernel'] = 'node_modules/lif-kernel';
-map['/lif-wallet'] = '.';
+map['/lif-kernel'] = './node_modules/lif-kernel';
+map['/lif-wallet'] = './';
 server({map, root});
