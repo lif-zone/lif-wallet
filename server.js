9fac2806f4adb287f8208c7578a0c8909e9da5e1
Bright Life <bright@bright.center>
2026-07-24 17:05:04 +0300

    update

--- server.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import server from 'lif-kernel/server_lib.js';
-let root = import.meta.dirname;
-let map = {};
-map['/lif-kernel'] = 'node_modules/lif-kernel';
-map['/lif-wallet'] = '.';
-server({map, root});
