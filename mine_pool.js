ae445993e2a6947bf1ab3599b6ea266bc335f158
LIF R&D <lif@lif.zone>
2026-09-17 23:53:52 +0300

    update

--- mine_pool.js
+++ mine_pool.js
@@ -100,7 +100,7 @@ function mine_steps_alt({pow, header, target, min, max}){
   };
   if (!be.init(events))
     return {error: 'failed be.init'};
-  be.setLoadRate(1);
+  be.set_r(1);
   be.start({header, target_n});
   et.on('finally', ()=>be.stop());
   return etask.wait();
